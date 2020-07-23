1a20c361846461a0fcc61e68084caf8d1acd8282
ThomasKatalenas <thomaskatalenas@ThomasKatalenass-MacBook-Air.local>
2020-07-23 19:48:57 -0400

    did classroom exercises

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,33 +13,59 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        self.vertices[v1].add(v2)
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+
+        q = Queue()
+        q.enqueue(starting_vertex)
+
+        visited = set()
+
+        while q.size() > 0: 
+            current_node = q.dequeue()
+            if current_node not in visited: 
+                visited.add(current_node)
+                neighbors = self.get_neightbors(current_node)
+                for neighbor in neighbors: 
+                    q.enqueue(neighbor)
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        s = Stack()
+        s.push(starting_vertex)
+        visited = Set()
+
+        while s.size > 0: 
+            current_node = s.pop()
+
+            if current_node not in visited: 
+                visited.add(current_node)
+
+                neighbors = self.get_neighbors(current_node)
+                for neighbor in neighbors: 
+                    s.push(neighbor)
+
+
 
     def dft_recursive(self, starting_vertex):
         """
